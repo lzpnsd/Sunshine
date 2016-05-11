9777bc20743406586c86cf5f92e657b11a030f46
liuqing <liuqing@sunniwell.net>
2016-05-11 10:06:53 +0800

    xml文件2次提交

--- src/com/lzpnsd/sunshine/view/MineView.java
+++ src/com/lzpnsd/sunshine/view/MineView.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MineView {
@@ -19,7 +19,7 @@ public class MineView {
 	private Context mContext;
 
 	private RelativeLayout mView;
-	private Button mBtnSettings;
+	private LinearLayout mLiSettings;
 	
 	public MineView(Context mContext) {
 		super();
@@ -38,8 +38,8 @@ public class MineView {
 	}
 
 	private void setView() {
-		mBtnSettings = (Button) mView.findViewById(R.id.btn_mine_settings);
-		mBtnSettings.setOnClickListener(mClickListener);
+		mLiSettings = (LinearLayout) mView.findViewById(R.id.li_settings_setting);
+		mLiSettings.setOnClickListener(mClickListener);
 	};
 	
 	private OnClickListener mClickListener = new OnClickListener() {
@@ -47,7 +47,7 @@ public class MineView {
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
-				case R.id.btn_mine_settings:
+				case R.id.li_settings_setting:
 					mContext.startActivity(new Intent(mContext,SettingsActivity.class));
 					break;
 			}
