470d5c0e1fb5522acf30c21088c60d7941f67ed0
liuqing <liuqing@sunniwell.net>
2016-05-13 16:19:49 +0800

    完善小组件代码提交

--- src/com/lzpnsd/sunshine/widget/provider/WidgetProvider.java
+++ src/com/lzpnsd/sunshine/widget/provider/WidgetProvider.java
@@ -1,15 +1,14 @@
 package com.lzpnsd.sunshine.widget.provider;
 
+import com.lzpnsd.sunshine.MainActivity;
 import com.lzpnsd.sunshine.R;
 import com.lzpnsd.sunshine.bean.CityBean;
 import com.lzpnsd.sunshine.bean.WeatherInfoBean;
 import com.lzpnsd.sunshine.bean.WidgetInfoBean;
 import com.lzpnsd.sunshine.manager.DataManager;
 import com.lzpnsd.sunshine.model.impl.WidgetInfoModel;
+import com.lzpnsd.sunshine.util.LogUtil;
 import com.lzpnsd.sunshine.util.WeatherIconUtil;
-import com.nostra13.universalimageloader.core.ImageLoader;
-
-import android.R.integer;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -17,9 +16,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -31,20 +27,17 @@ import android.widget.RemoteViews;
  * @author liuqing
  */
 public class WidgetProvider extends AppWidgetProvider {
-	private static final String TAG = "WidgetProvider";
+	private final LogUtil log = LogUtil.getLog(getClass());
 	private final Intent INTENT_SERVICE = new Intent("android.appwidget.action.WIDGET_SERVICE");
-	private final String UPDATA = "android.appwidget.action.APPWIDGET_UPDATE";
+	private final String ACTION_UPDATE_ALL = "com.lzpnsd.sunshine.widget.UPDATE_ALL";
+	
 	private Context mContext;
 	private WidgetInfoBean mWidgetInfoBean;
 	private WidgetInfoModel mWidgetInfoModel;
 	private WeatherInfoBean mWeatherInfoBean;
 	private CityBean mCityBean;
-	private Handler timeHandler;
 	private RemoteViews remoteView;
 	private static final int BUTTON_SHOW = 1;
-	private static final int TIME_START = 2;
-	private boolean isStop = true;
-	private int count = 0;
 
 	// 接收广播的回调函数
 	@Override
@@ -52,18 +45,21 @@ public class WidgetProvider extends AppWidgetProvider {
 		super.onReceive(context, intent);
 		mContext = context;
 		final String action = intent.getAction();
-		Log.d(TAG, "onReceive:action:" + action);
-		if (intent.hasCategory(Intent.CATEGORY_ALTERNATIVE)) {// 点击widget
+		log.d("onReceive:action:" + action);	
+		if(ACTION_UPDATE_ALL.equals(action)){
+			ShowWidgetInfo(mContext);
+		}else if(intent.hasCategory(Intent.CATEGORY_ALTERNATIVE)){//点击事件
 			Uri data = intent.getData();
 			int buttonId = Integer.parseInt(data.getSchemeSpecificPart());
-			Log.d(TAG, "buttonId="+buttonId);
+			log.d("buttonId=" + buttonId);
 			if (buttonId == BUTTON_SHOW) {
-				Log.d(TAG, "==Button widget goto apk==");
-				mContext.startService(INTENT_SERVICE);
+				log.d("==Button widget goto apk==");
+				Intent mIntent = new Intent();
+				intent.setComponent(new ComponentName(mContext,MainActivity.class));
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+				mContext.startActivity(intent);
 			}
-		}
-		if(intent.getAction().equals(UPDATA)){
-			ShowWidgetInfo(mContext);
 		}
 	}
 
@@ -71,75 +67,35 @@ public class WidgetProvider extends AppWidgetProvider {
 		mContext = context;
 		mWidgetInfoBean = new WidgetInfoBean();
 		mWidgetInfoModel = new WidgetInfoModel(mContext);
+		mWeatherInfoBean = DataManager.getInstance().getCurrentWeatherInfoBeans().get(1);
+		mCityBean = DataManager.getInstance().getCurrentCityBean();
+		mWidgetInfoBean = mWidgetInfoModel.getWidgetInfo();
+		String time = mWidgetInfoBean.getmWidgetTime();
+		String data = mWidgetInfoBean.getmWidgetData();
+		String month = mWidgetInfoBean.getmWidgetMouth();
+		String lowtem = mWeatherInfoBean.getLowTemperature();
+		String hightem = mWeatherInfoBean.getHighTemperature();
+		String weather = mWeatherInfoBean.getDayType();
+		int igweather = WeatherIconUtil.getDaySmallImageResource(weather);
+		String city = mCityBean.getNameCn();
+		// ImageLoader.getInstance().displayImage("drawable://"+WeatherIconUtil.getSmallImageResource(mWeatherInfoBean.getDayType()),
+		// R.id.iv_widget_weather);
+		log.d("time=" + time + " " + "data=" + data + " " + "month=" + month + "lowtem=" + lowtem + "hightem=" + hightem + "weather=" + weather + "city=" + city);
 
-		timeHandler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-					case TIME_START:
-						removeMessages(TIME_START);
-						mWeatherInfoBean = DataManager.getInstance().getCurrentWeatherInfoBeans().get(1);
-						mCityBean = DataManager.getInstance().getCurrentCityBean();
-						mWidgetInfoBean = mWidgetInfoModel.getWidgetInfo();
-						String time = mWidgetInfoBean.getmWidgetTime();
-						String data = mWidgetInfoBean.getmWidgetData();
-						String month = mWidgetInfoBean.getmWidgetMouth();
-						String lowtem = mWeatherInfoBean.getLowTemperature();
-						String hightem = mWeatherInfoBean.getHighTemperature();
-						String weather = null;
-						int igweather = 0;
-						if(WeatherIconUtil.isDayOrNight()){
-							weather = mWeatherInfoBean.getDayType();
-							igweather = WeatherIconUtil.getDaySmallImageResource(weather);
-						}else{
-							weather = mWeatherInfoBean.getNightType();
-							igweather = WeatherIconUtil.getNightSmallImageResource(weather);
-						}
-						String city = mCityBean.getNameCn();
-//						ImageLoader.getInstance().displayImage("drawable://"+WeatherIconUtil.getSmallImageResource(mWeatherInfoBean.getDayType()), R.id.iv_widget_weather);
-						Log.d(TAG, "time=" + time + " " + "data=" + data + " " + "month=" + month + "lowtem="
-+ lowtem + "hightem=" + hightem + "weather=" + weather + "city=" +city);
+		remoteView = new RemoteViews(mContext.getPackageName(), R.layout.widget_info_item);
+		remoteView.setTextViewText(R.id.tv_widget_time, time);
+		remoteView.setTextViewText(R.id.tv_widget_data, data);
+		remoteView.setTextViewText(R.id.tv_widget_month, month);
+		remoteView.setTextViewText(R.id.tv_widget_temperature, lowtem + "℃" + "~" + hightem + "℃");// 温度
+		remoteView.setTextViewText(R.id.tv_widget_weather, weather);// 天气
+		remoteView.setImageViewResource(R.id.iv_widget_weather, igweather);// 天气图片
+		remoteView.setTextViewText(R.id.tv_widget_area, city);// 城市
+		remoteView.setOnClickPendingIntent(R.id.Li_widget_layout, getPendingIntent(mContext, BUTTON_SHOW));
 
-						remoteView = new RemoteViews(mContext.getPackageName(), R.layout.widget_info_item);
-						remoteView.setTextViewText(R.id.tv_widget_time, time);
-						remoteView.setTextViewText(R.id.tv_widget_data, data);
-						remoteView.setTextViewText(R.id.tv_widget_month, month);
-						remoteView.setTextViewText(R.id.tv_widget_temperature,lowtem+"℃"+"~"+hightem+"℃");//温度
-						remoteView.setTextViewText(R.id.tv_widget_weather, weather);//天气
-						remoteView.setImageViewResource(R.id.iv_widget_weather, igweather);//天气图片
-						remoteView.setTextViewText(R.id.tv_widget_area, city);//城市
-						remoteView.setOnClickPendingIntent(R.id.Li_widget_layout, getPendingIntent(mContext, BUTTON_SHOW));
-						
-						AppWidgetManager manager = AppWidgetManager.getInstance(mContext);
-						int[] appWidgetIds = manager.getAppWidgetIds(new ComponentName(mContext, WidgetProvider.class));
-						manager.updateAppWidget(appWidgetIds, remoteView);// 更新所有实例
-						break;
+		AppWidgetManager manager = AppWidgetManager.getInstance(mContext);
+		int[] appWidgetIds = manager.getAppWidgetIds(new ComponentName(mContext, WidgetProvider.class));
+		manager.updateAppWidget(appWidgetIds, remoteView);// 更新所有实例
 
-					default:
-						break;
-				}
-			}
-		};
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				while(isStop){
-					count++;
-					Log.d(TAG, "count="+count);
-					Message timemsg = new Message();
-					timemsg.what = TIME_START;
-					timeHandler.handleMessage(timemsg);
-					try {
-						Thread.sleep(20000);// 20秒更新一次
-					} catch (InterruptedException e) {
-						// TODO: handle exception
-						e.getStackTrace();
-					}
-				}	
-			}
-		}).start();
 	}
 
 	private PendingIntent getPendingIntent(Context context, int buttonId) {
@@ -151,20 +107,13 @@ public class WidgetProvider extends AppWidgetProvider {
 		return pi;
 	}
 
-	// 每添加一次 widget 时，被执行
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		Log.d(TAG, "onUpdate():appWidgetIds.length==" + appWidgetIds.length);
-		mContext = context;
-		ShowWidgetInfo(mContext);
-	}
 
 	// 第一个widget被创建时调用
 	@Override
 	public void onEnabled(Context context) {
 		mContext = context;
-		Log.d(TAG, "onEnabled()");
-		ShowWidgetInfo(mContext);
+		log.d("onEnabled()");
+		mContext.startService(INTENT_SERVICE);
 		super.onEnabled(mContext);
 	}
 
@@ -172,16 +121,25 @@ public class WidgetProvider extends AppWidgetProvider {
 	@Override
 	public void onDisabled(Context context) {
 		mContext = context;
-		Log.d(TAG, "onDisabled()");
+		log.d("onDisabled()");
 		mContext.stopService(INTENT_SERVICE);
+//		mContext.stopService(JUMP_SERVICE);
 		super.onDisabled(mContext);
 	}
 
+	// 每添加一次 widget 时，被执行
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		log.d("onUpdate():appWidgetIds.length==" + appWidgetIds.length);
+		mContext = context;
+		super.onUpdate(mContext, appWidgetManager, appWidgetIds);
+	}
+	
 	// widget被删除时调用
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		mContext = context;
-		Log.d(TAG, "onDeleted():appWidgetIds.length=" + appWidgetIds.length);
+		log.d("onDeleted():appWidgetIds.length=" + appWidgetIds.length);
 		super.onDeleted(mContext, appWidgetIds);
 	}
 
